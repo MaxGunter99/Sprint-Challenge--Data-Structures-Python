c25c8959833252c021ba7de255b64dc6ff9096ef
maxgunter99 <maxgunter99@gmail.com>
2019-10-18 11:02:06 -0500

    added ring budder and names

--- names/names.py
+++ names/names.py
@@ -11,10 +11,36 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# # Works well!
+one = set( names_1 )
+
+dupes = one.intersection( names_2 )
+print( '\n\n' , list(dupes) , '\n\n' )
+duplicates.append( list(dupes) )
+
+duplicates = duplicates[0]
+
+# 0.00590205192565918 seconds
+
+# -----------------------------------------------
+
+# # 2nd
+# for i in names_1:
+#     if i in names_2:
+#         duplicates.append( i )
+# # 2.2562029361724854 seconds
+
+# -----------------------------------------------
+
+# # original
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# # 9.02682900428772 seconds
+
+print( '\n\n' , duplicates )
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,48 @@
+import os
+os.system( 'clear' )
+
 class RingBuffer:
   def __init__(self, capacity):
+
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    
+    print( '\n' )
+    
+    # If storage is full
+    if self.current == len( self.storage ):
+
+      print( '--- Storage is full ---' )
+
+      # Reset index / counter
+      self.current -= len( self.storage )
+
+      # Start popping values and replacing them with new ones ( counter / current increases )
+      self.storage.pop( self.current )
+      self.storage.insert( self.current , item )
+      self.current += 1
+
+    # If storage is not full
+    else:
+
+      # Start popping values and replacing them with new ones ( counter / current increases )
+      print( f'Adding: { item } ' )
+      self.storage.pop( self.current )
+      self.storage.insert( self.current , item )
+      self.current += 1
 
   def get(self):
-    pass
+
+    answer = []
+
+    for i in range( len( self.storage ) ):
+
+      if self.storage[ i ] is not None:
+        answer.append( self.storage[i] )
+
+    print( f'Returning: { answer } ' )
+
+    return answer
